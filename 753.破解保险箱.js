f7fe8af334ab129c9806f65f11d97a4cdc848b8c
moewang0321 <107977176@qq.com>
2020-06-03 13:09:16 +0800

    K个一组反转链表

---
+++ 753.破解保险箱.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=753 lang=javascript
+ *
+ * [753] 破解保险箱
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {string}
+ */
+var crackSafe = function(n, k) {
+
+};
+// @lc code=end
+
